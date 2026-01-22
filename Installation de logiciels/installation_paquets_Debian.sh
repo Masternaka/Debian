ec5ede2c92b6b684e2cf203798160dbd6868782f
Masternaka <gabthesniper@gmail.com>
2026-01-22 15:38:33 -0500

    update

--- Installation de logiciels/installation_paquets_Debian.sh
+++ Installation de logiciels/installation_paquets_Debian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
 
 # Vérifier si l'utilisateur est root
-if [ "$EUID" -ne 0 ]
-then 
+if [ "$EUID" -ne 0 ]; then 
   echo "Veuillez exécuter ce script en tant que root (sudo)."
-  exit
+  exit 1
 fi
 
 # Mettre à jour les dépôts et les paquets existants
@@ -13,28 +13,36 @@ apt update && apt upgrade -y
 
 # Liste des paquets à installer via apt
 apt_packages=(
-  vim
+  micro
   git
   curl
-  htop
-  firefox-esr
-  build-essential
-  zsh
-  neofetch
-  terminator
-  tmux
+  wget
+  btop
+  fastfetch
+  kitty
+  lshw
+  fwupd
+  p7zip-full
+  meld
 )
 
 # Installation des paquets avec apt
 echo "Installation des paquets via apt..."
-for package in "${apt_packages[@]}"
-do
-  if dpkg -l | grep -q $package
-  then
+for package in "${apt_packages[@]}"; do
+  if dpkg -s "$package" &> /dev/null; then
     echo "$package est déjà installé."
   else
-    apt install -y $package
+    echo "Installation de $package..."
+    if apt install -y "$package"; then
+      echo "$package installé avec succès."
+    else
+      echo "Erreur lors de l'installation de $package."
+      exit 1
+    fi
   fi
 done
 
-echo "Installation terminée!"
+echo "Nettoyage des paquets inutiles..."
+apt autoremove -y
+
+echo "Installation terminée !"
